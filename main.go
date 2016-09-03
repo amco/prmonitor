4219f49c5d5e85b92fdb730691cea28171d9c2e4
Brent Rich <bdrich.mail@gmail.com>
2016-09-02 23:19:02 -0700

    Gather and display useful data.
    I'm interested in displaying a dashboard showing the current state of
    pull requests in an org, with maybe some historical context. The key
    variables I'm interested in are duration (time spent in review) and
    depth (number of PRs outstanding at a given time).
    
    There are two options for getting the data: (1) indexing it by
    spidering through all the pages or (2) getting the most recent PRs
    up to an arbitrary limit- like the first page. I've decided to go
    with 2 because its much less complex and the default of 30 per page
    should get plenty of historical data for most of the projects I work
    with. This commit implements option 2 and dumps out both opened and
    closed PRs along with the key variables.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
+	"time"
 )
 
 type Config struct {
@@ -34,10 +35,57 @@ func main() {
 	}
 
 	client := github.NewClient(tp.Client())
-	prs, _, err := client.PullRequests.List(t.Owner, t.Repo, nil)
+
+	// get 30 latest closed pull requests.
+	op := &github.PullRequestListOptions{}
+	op.State = "open"
+	op.Sort = "created"
+	op.Direction = "desc"
+	op.PerPage = 30
+	op.Page = 0
+	oprs, _, err := client.PullRequests.List(t.Owner, t.Repo, op)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v", prs)
+	// get 30 latest closed pull requests.
+	cp := &github.PullRequestListOptions{}
+	cp.State = "closed"
+	cp.Sort = "created"
+	cp.Direction = "desc"
+	cp.PerPage = 30
+	cp.Page = 0
+	cprs, _, err := client.PullRequests.List(t.Owner, t.Repo, cp)
+
+	// make display a bit more readable
+	fmt.Println("open prs:")
+	for _, v := range oprs {
+		var start, end time.Time
+		if v.CreatedAt == nil {
+			panic(fmt.Errorf("no createdat date"))
+		} else {
+			start = *v.CreatedAt
+		}
+		if v.ClosedAt == nil {
+			end = time.Now()
+		} else {
+			end = *v.ClosedAt
+		}
+		fmt.Printf("  #%d %s [%s to %s]\n", *v.Number, *v.Title, start.Format(time.RFC3339), end.Format(time.RFC3339))
+	}
+	fmt.Println("closed prs:")
+	for _, v := range cprs {
+		var start, end time.Time
+		if v.CreatedAt == nil {
+			panic(fmt.Errorf("no createdat date"))
+		} else {
+			start = *v.CreatedAt
+		}
+		if v.ClosedAt == nil {
+			end = time.Now()
+		} else {
+			end = *v.ClosedAt
+		}
+		fmt.Printf("  #%d %s [%s to %s]\n", *v.Number, *v.Title, start.Format(time.RFC3339), end.Format(time.RFC3339))
+	}
 }
