f51a96efbf26b8051a0aa2547717e9bdd40ca790
Brent Rich <bdrich.mail@gmail.com>
2016-09-03 01:25:06 -0700

    Add styling to dashboard
    This commit adds a dark theme and shows PR duration as a bar graph,
    with critical points (>1 day, >3 days) highlighted with different
    colors.

--- main.go
+++ main.go
@@ -66,13 +66,14 @@ func dashboard(w http.ResponseWriter, r *http.Request) {
 		op.Direction = "desc"
 		op.PerPage = r.Depth
 		op.Page = 0
+		op.Base = "master"
 		oprs, _, err := client.PullRequests.List(r.Owner, r.Repo, op)
 		if err != nil {
 			panic(err)
 		}
 
 		// make display a bit more readable
-		fmt.Fprintf(w, "<b>%s/%s</b><ul>", r.Owner, r.Repo)
+		fmt.Fprintf(w, "<html><body style='background: #333; color: #fff;'>")
 		for _, v := range oprs {
 			start := *v.CreatedAt
 			user := *v.User
@@ -81,15 +82,36 @@ func dashboard(w http.ResponseWriter, r *http.Request) {
 				for _, a := range *r.Authors {
 					if *user.Login == a {
 						// print for single author
-						fmt.Fprintf(w, "<li>#%d %s by %s @ %s</li>", *v.Number, *v.Title, *user.Login, start.Format(time.RFC3339))
+						render(w, r.Owner, r.Repo, *v.Number, *v.Title, *user.Login, time.Since(start))
 					}
 				}
 			} else {
 				// match all
-				fmt.Fprintf(w, "<li>#%d %s by %s @ %s</li>", *v.Number, *v.Title, *user.Login, start.Format(time.RFC3339))
+				render(w, r.Owner, r.Repo, *v.Number, *v.Title, *user.Login, time.Since(start))
 			}
 		}
-		fmt.Fprintf(w, "</ul>")
+		fmt.Fprintf(w, "</body></html>")
+	}
+}
+
+func render(w http.ResponseWriter, owner string, repo string, number int, title string, author string, hours time.Duration) {
+	n := hours.Hours() / (240*time.Hour).Hours()
+	if n > 1 {
+		n = 1
 	}
 
+	stopyellow := (24*time.Hour).Hours()
+
+	stopred := (24*3*time.Hour).Hours()
+
+	color := "#777"
+	if hours.Hours() > stopred {
+		color = "#FF4500"
+	} else if hours.Hours() > stopyellow {
+		color = "#FFA500"
+	}
+
+
+	style := fmt.Sprintf(`margin: 3px; padding: 8px; background: linear-gradient( 90deg, %s %d%%, #333 %d%%);`, color, int(n*100), int(n*100))
+	fmt.Fprintf(w, "<div style='%s'><b>%s/%s</b> #%d %s by %s @ %d days or %d hours</div>", style, owner, repo, number, title, author, hours/(24*time.Hour), hours/time.Hour)
 }
